b85ee234a0ece1edde4eb45be4de9093eae7e7ed
sanchit-sehgal <76457106+sanchit-sehgal@users.noreply.github.com>
2021-02-24 18:26:26 -0500

    Create debian-dependencies.sh

---
+++ debian-dependencies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo su 
+
+sudo apt-get install python3
+sudo apt-get install notify-osd
+apt-get install libnotify-bin
+sudo apt-get install -y w3m
+sudo apt-get install sl
+sudo apt-get install curl
+sudo apt-get install nudoku
+sudo apt-get install bastet
+sudo apt-get install pydf
+sudo apt-get install hwinfo
+
+pip install pyautogui
+pip install PyDictionary
+pip install pynput
+pip install pygame
+
+exit 
