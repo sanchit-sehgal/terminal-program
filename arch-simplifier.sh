a0f8f1876eebc4505115c2cbc5d8b8c2db73bfdf
sanchit-sehgal <76457106+sanchit-sehgal@users.noreply.github.com>
2021-02-24 18:21:35 -0500

    Update arch-simplifier.sh

--- arch-simplifier.sh
+++ arch-simplifier.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+echo "Installing Dependencies... "
+sudo su
 sudo pacman -S python --noconfirm
 sudo pacman -S tk --noconfirm
 sudo pacman -S tensorflow --noconfirm
 sudo pacman -S w3m
 sudo pacman -S curl
-sudo pacman -Syy && sudo pacman -Syu --noconfirm
-
+sudo pacman -S nnn
+sudo pacman -S sl
+git clone https://aur.archlinux.org/nudoku-git.git
+cd nudoku-git
+makepkg -si --noconfirm
+cd -
+git clone https://aur.archlinux.org/bastet.git
+cd bastet
+makepkg -si --noconfirm
+cd -
+sudo pacman -S pydf
+sudo pacman -S 
 pip install pyautogui
 pip install PyDictionary
 pip install pynput
+pip install pygame
+exit
