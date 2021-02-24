93ffd5e59fdfec1ea6b608025b2d31d60e0dd651
sanchit-sehgal <76457106+sanchit-sehgal@users.noreply.github.com>
2021-02-24 17:38:25 -0500

    Update simplifier.sh

--- simplifier.sh
+++ simplifier.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+sudo pacman -S git --noconfirm
+sudo pacman -S python --noconfirm
+sudo pacman -S tk --noconfirm
+sudo pacman -S tensorflow --noconfirm
+sudo pacman -Syy && sudo pacman -Syu --noconfirm
+
+pip install pyautogui
+pip install PyDictionary
+pip install pynput
+
 cd main
 python runner.py
